b9ef34078812ea1389b058f5375d7ddb491ac605
doswald32 <dan.oswald32@gmail.com>
2024-05-20 18:08:53 -0600

    completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves_list):
+    num = 1
+    for name in dwarves_list:
+        print(f'{num}. {name}')
+        num += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list):
+    new_list = []
+    for item in list:
+        new_list.append(f'{item.capitalize()}!')
+    return new_list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    for item in list:
+        if item == "cheddar" or item == "gouda" or item == "camembert":
+            return item
+    return None
